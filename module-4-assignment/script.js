550aac68786d5ff7b1d4be2b7837e0f9578896d3
priyanka-singhh <priyanka.17bcs1183@abes.ac.in>
2020-07-02 14:45:04 +0530

    mod 4 solution

--- module-4-assignment/script.js
+++ module-4-assignment/script.js
@@ -1,10 +1,10 @@
 (function(window) {
-	var names=["Pri","Jhon","Ritu", "Juhi","Jim","Ani","Manvi"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	
 	for(var i in names){
 
 		var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-		if(firstLetter== 'j'){
+		if(firstLetter == 'j'){
 			window.goodSpeaker.speak(names[i]);
 		}
 		else{
